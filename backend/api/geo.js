30c1ba1b87fbbe2900dbed3ce136f15497cfc01b
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-09-01 20:51:09 -0400

    Delete backend/api/geo.js

--- backend/api/geo.js
+++
@@ -1,7 +0,0 @@
-// Geo endpoints disabled; minimal stub
-const express = require('express');
-const router = express.Router();
-router.all('*', (_req, res) => {
-  res.status(410).json({ success: false, error: 'Geo endpoints are disabled' });
-});
-module.exports = router;
